b57c1b9a69bae205352c13494fda12008751d99f
Galibri <galib.bcse@gmail.com>
2019-04-17 10:49:00 +0600

    Create, show, delete questions using api and add QuestionResource to filter out the transmitted data

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,25 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\QuestionResource;
 use App\Model\Question;
+use Exception;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class QuestionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        $questions = Question::latest()->get();
+        return QuestionResource::collection($questions);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -30,30 +35,35 @@ class QuestionController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
+//        auth()->user()->question()->create($request->all());
+        Question::create($request->all());
+        return response()->json([
+            'status' => Response::HTTP_CREATED,
+            'created' => true,
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param Question $question
+     * @return QuestionResource
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param Question $question
+     * @return Response
      */
     public function edit(Question $question)
     {
@@ -63,9 +73,9 @@ class QuestionController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Question $question
+     * @return void
      */
     public function update(Request $request, Question $question)
     {
@@ -75,11 +85,13 @@ class QuestionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param Question $question
+     * @return Response
+     * @throws Exception
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    //    protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+    protected $guarded = [];
+    /**
+     * @return string
+     * Change the Questions url to slug instead of id
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
@@ -29,5 +40,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
